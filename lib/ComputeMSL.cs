c56bab9a33b3b50839db08ef00afd8eb083890c1
Jabes Pauya <j.pauya@b-eee.com>
2019-07-09 19:10:12 +0900

    changes and update

--- lib/ComputeMSL.cs
+++ lib/ComputeMSL.cs
@@ -1,24 +1,24 @@
-using System;
-using GeoidHeightsDotNet;
-using geonet.interfaces;
+using System.Runtime.InteropServices;
 using geonet.helpers;
+using RGiesecke.DllExport;
 
 namespace geonet.lib
 {
-    public class ComputeMSL : IComputeMSL
+    public class ComputeMSL 
     {
-        public ComputeMSL(){}
-
         /* GetMSLResult from the parameter as lat, lon, and gps elevation */
-        public double GetMSLResult(double lat, double lon, double gpsElevation) =>
+        [DllExport]
+        public static double GetMSLResult(double lat, double lon, double gpsElevation) =>
             Helper.GetMSL(Helper.GetGeoID(lat, lon), gpsElevation);
 
         /* GetWSG84Result from the parameter gpsElevation and mslElevation */
-        public double GetWSG84Result(double gpsElevation, double mslElevation) =>
+        [DllExport]
+        public static double GetWSG84Result(double gpsElevation, double mslElevation) =>
             Helper.ConvertMSLToWSG84(gpsElevation, mslElevation);
 
          /* GetGeoID from lat and lon */
-        public double GetGeoID(double lat, double lon) => 
+        [DllExport]
+        public static double GetGeoID(double lat, double lon) => 
             Helper.GetGeoID(lat, lon);
     }
 }
