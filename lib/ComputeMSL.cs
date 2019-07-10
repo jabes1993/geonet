42976653f481869db8aa2af296610cf0b1fc037f
Jabes Pauya <j.pauya@b-eee.com>
2019-07-10 12:21:38 +0900

    added name

--- lib/ComputeMSL.cs
+++ lib/ComputeMSL.cs
@@ -1,23 +1,21 @@
-using System.Runtime.InteropServices;
 using geonet.helpers;
-using RGiesecke.DllExport;
 
-namespace geonet.lib
+namespace geonet 
 {
-    public class ComputeMSL 
+    public class ComputeMSL
     {
         /* GetMSLResult from the parameter as lat, lon, and gps elevation */
-        [DllExport]
+        [DllExport("GetMSLResult", CallingConvention = System.Runtime.InteropServices.CallingConvention.ThisCall)]
         public static double GetMSLResult(double lat, double lon, double gpsElevation) =>
             Helper.GetMSL(Helper.GetGeoID(lat, lon), gpsElevation);
 
         /* GetWSG84Result from the parameter gpsElevation and mslElevation */
-        [DllExport]
+        [DllExport("GetWSG84Result", CallingConvention = System.Runtime.InteropServices.CallingConvention.ThisCall)]
         public static double GetWSG84Result(double gpsElevation, double mslElevation) =>
             Helper.ConvertMSLToWSG84(gpsElevation, mslElevation);
 
          /* GetGeoID from lat and lon */
-        [DllExport]
+         [DllExport("GetGeoID", CallingConvention = System.Runtime.InteropServices.CallingConvention.ThisCall)]
         public static double GetGeoID(double lat, double lon) => 
             Helper.GetGeoID(lat, lon);
     }
